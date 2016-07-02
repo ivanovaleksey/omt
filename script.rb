201af5a552567e5f23d34c8d36b45ba51b2e15ac
Aleksey Ivanov <ialexxei@gmail.com>
2016-07-03 02:38:23 +0300

    generate excel file

--- script.rb
+++ script.rb
@@ -1,11 +1,15 @@
 #! /usr/bin/env ruby
+# -*- coding: utf-8 -*-
 
 require 'active_support'
 require 'active_support/core_ext'
+require 'awesome_print'
+require 'axlsx'
 require 'base64'
 require 'dotenv'
 require 'faraday'
 require 'json'
+require 'yaml'
 
 Dotenv.load
 
@@ -67,9 +71,16 @@ def document_states
   end
 end
 
+def document_types
+  @document_types ||= API.get('tip_documents').each_with_object({}) do |type, hash|
+    hash[type['ID_TIP_DOCUMENT']] = type['TIP_DOCUMENT']
+  end
+end
+
 def deals
-  @deals ||= API.get('deal').map do |deal|
+  @deals ||= API.get('deal').each_with_object({}) do |deal, hash|
     merge_association deal, :deal_types, :id_type_deal, :type
+    hash[deal['id_deal']] = deal
   end
 end
 
@@ -90,9 +101,11 @@ def additional_fields
   end
 end
 
-def merge_association(object, collection, key, name)
+def merge_association(object, collection, key, name = nil)
   id = key.is_a?(Symbol) ? object.delete(key.to_s) : key
-  object.merge! name.to_s => send(collection)[id]
+  hash = name ? ({ name.to_s => send(collection)[id] }) : send(collection)[id]
+  object.merge! hash if hash
+  object
 end
 
 # ========= ASSOCIATIONS =========
@@ -100,16 +113,19 @@ end
 def documents
   @documents ||= begin
     API.get('documents', document_params).map do |doc|
-      merge_association doc, :customers,         :companies_id, :customer
-      merge_association doc, :users,             :id_manager,   :manager
-      merge_association doc, :additional_fields, doc['id_doc'], :fields
+      merge_association doc, :customers,         :companies_id,   :customer
+      merge_association doc, :users,             :id_manager,     :manager
+      merge_association doc, :deals,             :id_deal,        :deal
+      merge_association doc, :document_types,    :id_tip_doc,     :type
+      merge_association doc, :document_states,   :id_priznzk_doc, :state
+      merge_association doc, :additional_fields, doc['id_doc']
     end
   end
 end
 
 def document_params
   {
-    '$select'  => 'id_doc, companies_id, id_manager, id_priznzk_doc, summa, number_doc, data_doc, tovar_doc',
+    '$select'  => 'id_doc, companies_id, id_manager, id_tip_doc, id_priznzk_doc, id_deal, summa, number_doc, data_doc, tovar_doc',
     '$expand'  => 'tovar_doc',
     '$orderby' => 'data_doc',
     '$filter'  => date_filter
@@ -118,17 +134,94 @@ end
 
 def date_filter
   [
-    # "data_doc ge datetime'#{dates[:from]}'",
-    "data_doc ge datetime'2016-06-27T18:19:22.527+03:00'",
+    "data_doc ge datetime'#{dates[:from]}'",
     "data_doc le datetime'#{dates[:to]}'"
   ].join(' and ')
 end
 
 def dates
   {
-    from: ENV['DATE_FROM'].presence || Time.now.beginning_of_week.to_s(:iso8601),
-    to:   ENV['DATE_TO'].presence || Time.now.end_of_week.to_s(:iso8601)
+    from: (ENV['DATE_FROM']&.to_time || Time.now.beginning_of_week).to_s(:iso8601),
+    to:   (ENV['DATE_TO']&.to_time || Time.now.end_of_week).to_s(:iso8601)
   }
 end
 
-p documents.last 2
+def config
+  @config ||= YAML.load File.open('config.yml')
+end
+
+def headers
+  @headers ||= config['headers']
+end
+
+def document_rows(document)
+  document['tovar_doc']['results'].map do |product|
+    [
+      document['number_doc'],
+      format_date(document['data_doc']),
+      document['type'],
+      document['state'],
+      document['summa'],
+      document['paid'],
+      format_date(document['ADD_LIST_DOCUMENTS_N33']),
+      format_date(document['ADD_LIST_DOCUMENTS_N34']),
+      document['customer']['name'],
+      document['manager']['name'],
+      deal_label(document['deal']),
+      document['customer']['type']['descr'],
+      product['tovar'],
+      product['kol_vo'],
+      product['price'],
+      product['summa']
+    ]
+  end
+rescue
+  p document
+  raise
+end
+
+def format_date(date)
+  return unless date
+  Date.parse(date).strftime('%d/%m/%Y')
+end
+
+def deal_label(deal)
+  return unless deal
+  [deal['date_start'].to_date, deal['type'], deal['id_deal']].join ' '
+end
+
+def define_styles(sheet)
+  header = sheet.styles.add_style bg_color: '5B9BD5', fg_color: 'FFFFFF', sz: 10, b: true
+  row = sheet.styles.add_style border: { style: :thin, color: '5B9BD5', edges: [:top] }
+  @styles = { header: header, row: row }
+end
+
+def filename
+  format '%{name}-%{date}.%{ext}',
+         name: 'export',
+         date: Date.today.strftime('%d-%m-%Y'),
+         ext: 'xlsx'
+end
+
+def write_file
+  Axlsx::Package.new do |p|
+    wb = p.workbook
+    wb.add_worksheet do |sheet|
+      define_styles sheet
+      sheet.add_row headers, style: @styles[:header]
+
+      documents.each do |document|
+        document_rows(document).each do |row|
+          sheet.add_row row, style: @styles[:row]
+        end
+      end
+    end
+    p.serialize filename
+  end
+end
+
+def call
+  write_file
+end
+
+call
